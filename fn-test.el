770da4c435735b57b563da1ff67b1a2ecd32d199
Troy Pracy <troyp7@gmail.com>
2017-01-30 18:51:43 +1100

    remove tests for functions no longer in package

--- fn-test.el
+++ fn-test.el
@@ -51,53 +51,6 @@
 
     )
 
-  (ert-deftest test-fn-bindl ()
-    "Test `fn-bindl'."
-
-    (should-equal
-     (mapcar (fn-bindl '- 10)
-             '(1 2 3 4 5 6 7 8 9 10))
-     ;; result:
-     '(9 8 7 6 5 4 3 2 1 0))
-
-    )
-
-  (ert-deftest test-fn-bindr ()
-    "Test `fn-bindr'."
-
-    (should-equal
-     (mapcar (fn-bindl '- 10)
-             '(1 2 3 4 5 6 7 8 9 10))
-     ;; result:
-     '(9 8 7 6 5 4 3 2 1 0))
-
-    )
-
-  (ert-deftest test-fn-fork ()
-    "Test `fork'."
-
-    (should-equal
-     (fn-call (fn-fork 'list '+ '*) 2 3 5)
-     ;; result:
-     '(10 30))
-
-    (should-eql
-     (fn-call (fn-fork '/ '-sum 'length)
-              '(2.0 0 2.0 3.0 5.0 1.0 8.0))
-     ;; result:
-     3.0)
-
-    (should-equal
-     (-map (fn-fork '+
-                    (fn (expt <> 5))
-                    (fn <>)
-                    (fn 1))
-           '(1 2 10))
-     ;; result:
-     '(3 35 100011))
-
-    )
-
   )
 
 (defun fn---test-all ()
