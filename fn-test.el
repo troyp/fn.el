70067fcf5e2447c9d7415cee3d433a8e974f012e
Troy Pracy <troyp7@gmail.com>
2017-01-30 19:06:05 +1100

    change test to use supported syntax

--- fn-test.el
+++ fn-test.el
@@ -14,7 +14,7 @@
   (ert-deftest test-fn ()
     "Test `fn'."
 
-    (should-equal (-map (fn (* <> <>))
+    (should-equal (-map (fn (* <1> <1>))
                         '(0 1 2 3 4 5 6 7 8 9 10))
                   ;; result:
                   '(0 1 4 9 16 25 36 49 64 81 100))
